fb1b80a7f15653d42eaf342aa8d0f4b4cac37e91
Betoside <betoside@gmail.com>
2023-07-13 02:09:08 -0300

    finalizado module7, exercicio Generate Excuse

--- src/App.js
+++ src/App.js
@@ -1,34 +1,35 @@
 import './App.css';
 import Axios from 'axios';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 
 function App() {
 
   // const toolAPI = 'https://jsonplaceholder.typicode.com/posts';
   // const toolAPI = 'https://catfact.ninja/fact';
-  const toolAPI = 'https://api.agify.io/?name=';
+  // const toolAPI = 'https://api.agify.io/?name=';
+  // const toolAPI = 'https://coffee.alexflipnote.dev/random.json';
 
-  const [name, setName] = useState("");
-  const [predictAge, setPredictAge] = useState(null);
+  const [desculpa, setDesculpa] = useState('');
 
-  const fetchData = (nome) => {
-    Axios.get(toolAPI+name).then((res)=>{
-      setPredictAge(res.data);
-    })
-  };
+  const fetchDesculpa = (categoria) => {
+    Axios.get(`https://excuser-three.vercel.app/v1/excuse/${categoria}/`).then(
+      (res) => {
+        setDesculpa(res.data[0]);
+      }
+    );
+  }
 
   return (
-    <div className="App">
-      <input 
-        type="text" 
-        placeholder='Ex. Pedro...'
-        onChange={(event) => {setName(event.target.value)}} />
-      <br />
-      <br />
-      <button onClick={fetchData}>Predict Age</button>
-      <p>Name: {predictAge?.name}</p>
-      <p>Age: {predictAge?.age}</p>
-      <p>Count: {predictAge?.count}</p>
+    <div className="App generateExcuse">
+      <h3>Generate An Excuse</h3>
+      <button onClick={() => {fetchDesculpa('party')}}>Party</button>
+      <button onClick={() => {fetchDesculpa('family')}}>Family</button>
+      <button onClick={() => {fetchDesculpa('office')}}>Office</button>
+      <div className='desculpa'>
+        <div className='id'>id: {desculpa.id}</div>
+        <div className='excuse'>{desculpa.excuse}</div>
+        <div className='category'>categoria: {desculpa.category}</div>
+      </div>
     </div>
   );
 }
